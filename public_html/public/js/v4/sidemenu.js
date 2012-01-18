051e9ad86203169082c139125ae539d01057a31d
Ramin Vakilian <ramin@motionworks.com.my>
2012-01-18 16:49:52 +0800

    cookie set fix

--- public_html/public/js/v4/sidemenu.js
+++ public_html/public/js/v4/sidemenu.js
@@ -56,7 +56,7 @@ function setCookie(c_name,value,exdays)
 {
 	var exdate=new Date();
 	exdate.setDate(exdate.getDate() + exdays);
-	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString()) + ";path=/";
+	var c_value=escape(value) + "; path=/" + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
 	document.cookie=c_name + "=" + c_value;
 }
 function getCookie(c_name)
