b04534422aad2f4e11bb9c9dba61aa065ceab8bf
Reza <reza@motionworks.com.my>
2012-07-20 17:21:42 +0800

    working on mymission page

--- public_html/application/views/mybids_codearmy_view.php
+++ public_html/application/views/mybids_codearmy_view.php
@@ -1,5 +1,28 @@
 <?php $this->load->view('includes/CAProfileHeader.php'); ?>
 
-my bids
-
+<div id="mybids-content-area"> 
+  
+  <!-- START - Mission Bid Title Block - Dev. by Reza  -->
+  <div id="block-mission-bid-header">
+    <div class="block-header">
+      <h3>Mission Bid</h3>
+    </div>
+    <div id="mission-bid-header-text"> You've placed a bid for </div>
+  </div>
+  <div id="block-bid-1">
+    <div id="bid-date">19th May 2012</div>
+    <div id="bid-info-block">
+    
+    <div id=""
+      <div id="bid-close-date">Bid closes on 22nd May 2012</div>
+      <div id="proj-title">open, rename, save and delete site diagrams</div>
+    </div>
+  </div>
+  
+  
+  <div id="block-bid-2"> </div>
+  <div id="block-bid-3"> </div>
+  <div id="block-bid-4"> </div>
+  <div id="block-bid-5"> </div>
+</div>
 <?php $this->load->view('includes/CAProfileFooter.php'); ?>
