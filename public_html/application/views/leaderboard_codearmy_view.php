a52a1ab253c74dab2043450c637b0b6df80726f6
Reza <reza@motionworks.com.my>
2012-07-27 10:05:02 +0800

    leaderboard slicing done

--- public_html/application/views/leaderboard_codearmy_view.php
+++ public_html/application/views/leaderboard_codearmy_view.php
@@ -1,3 +1,47 @@
 <?php $this->load->view('includes/CAProfileHeader.php'); ?>
-<img src="/public/images/codeArmy/profile/leaderboard/temp.png" id="preview"/>
+
+<div id="leaderboard-content-area"> 
+  
+  <!-- START - Mission Bid Title Block - Dev. by Reza  -->
+  <div id="block-leaderboard-header">
+    <div class="block-header">
+      <h3>Leaderboard</h3>
+    </div>
+    <div id="leaderboard-header-text"> Go above and beyond the call of duty to earn special awards.</div>
+  </div>
+  <div id="leaderboard-cols">
+    <div id="leaderboard-left-col">
+      <div id="col-title"> Points Gained </div>
+      <div id="col-content">
+        <ol>
+          <li>
+            <div id="col-li">
+              <div id="col-li-left"><img src="public/images/codeArmy/profile/leaderboard/avatar-temp.jpg" /></div>
+              <div id="col-li-right"><span id="name">Ramin</span> <br />
+                <span id="level">Serrgent 5 Star</span> <br />
+                <span id="points">3276 Points</span></div>
+            </div>
+          </li>
+        </ol>
+      </div>
+      <div id="col-footer"> </div>
+    </div>
+    <div id="leaderboard-right-col">
+      <div id="col-title"> PHP Level </div>
+      <div id="col-content">
+        <ol>
+          <li>
+            <div id="col-li">
+              <div id="col-li-left"><img src="public/images/codeArmy/profile/leaderboard/avatar-temp.jpg" /></div>
+              <div id="col-li-right"><span id="name">Ramin</span> <br />
+                <span id="level">Serrgent 5 Star</span> <br />
+                <span id="points">3276 Points</span></div>
+            </div>
+          </li>
+        </ol>
+      </div>
+      <div id="col-footer"> </div>
+    </div>
+  </div>
+</div>
 <?php $this->load->view('includes/CAProfileFooter.php'); ?>
