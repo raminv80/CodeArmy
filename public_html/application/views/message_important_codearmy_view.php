a195ecd142ed262040ac1fc454b6bba1f263ce1c
Reza <reza@motionworks.com.my>
2012-07-27 16:23:14 +0800

    msg section

--- public_html/application/views/message_important_codearmy_view.php
+++ public_html/application/views/message_important_codearmy_view.php
@@ -1,3 +1,74 @@
 <?php $this->load->view('includes/CAProfileHeader.php'); ?>
 
+<div id="inbox-content-area"> 
+  
+  <!-- START - Inbox Block - Dev. by Reza  -->
+  <div id="block-inbox-header">
+    <div class="block-header">
+      <h3>Messages</h3>
+    </div>
+  </div>
+  <div id="inbox-area">
+    <div id="inbox-top-bar">
+      <div id="back"><a href="#">Back</a></div>
+      <div id="mark-unread"><a href="#">Mark as unread</a></div>
+      <div id="star"><a href="#"><img src="/public/images/codeArmy/messages/star.png" /></a></div>
+      <div id="bin"><a href="#"><img src="/public/images/codeArmy/messages/bin.png" /></a></div>
+      <div id="msg-no">1 of 5</div>
+      <div id="arrow-left"><a href="#"><img src="/public/images/codeArmy/messages/arrow-left.png" /></a></div>
+      <div id="arrow-right"><a href="#"><img src="/public/images/codeArmy/messages/arrow-right.png" /></a></div>
+    </div>
+    <div id="inbox-rows">
+      <div id="title-row">
+        <div id="select-all"><a href="#">Select all</a></div>
+        <div id="all-messages">All messages</div>
+      </div>
+      <div id="msg-row">
+        <input type="checkbox" />
+        <div id="inbox-user-avatar"><a href="#"><img src="/public/images/codeArmy/messages/default-avatar.png" /></a></div>
+        <div id="sender-name"><a href="#">Asghar</a></div>
+        <div id="mail-subject"><a href="#">Nice to meet you!</a></div>
+        <div id="mail-time">3 hours ago</div>
+        <div id="star"><a href="#"><img src="/public/images/codeArmy/messages/star.png" /></a></div>
+        <div id="bin"><a href="#"><img src="/public/images/codeArmy/messages/bin.png" /></a></div>
+      </div>
+      <div id="msg-row">
+        <input type="checkbox" />
+        <div id="inbox-user-avatar"><a href="#"><img src="/public/images/codeArmy/messages/default-avatar.png" /></a></div>
+        <div id="sender-name"><a href="#">Asghar</a></div>
+        <div id="mail-subject"><a href="#">Nice to meet you!</a></div>
+        <div id="mail-time">3 hours ago</div>
+        <div id="star"><a href="#"><img src="/public/images/codeArmy/messages/star.png" /></a></div>
+        <div id="bin"><a href="#"><img src="/public/images/codeArmy/messages/bin.png" /></a></div>
+      </div>
+      <div id="msg-row">
+        <input type="checkbox" />
+        <div id="inbox-user-avatar"><a href="#"><img src="/public/images/codeArmy/messages/default-avatar.png" /></a></div>
+        <div id="sender-name"><a href="#">Asghar</a></div>
+        <div id="mail-subject"><a href="#">Nice to meet you!</a></div>
+        <div id="mail-time">3 hours ago</div>
+        <div id="star"><a href="#"><img src="/public/images/codeArmy/messages/star.png" /></a></div>
+        <div id="bin"><a href="#"><img src="/public/images/codeArmy/messages/bin.png" /></a></div>
+      </div>
+      <div id="msg-row">
+        <input type="checkbox" />
+        <div id="inbox-user-avatar"><a href="#"><img src="/public/images/codeArmy/messages/default-avatar.png" /></a></div>
+        <div id="sender-name"><a href="#">Asghar</a></div>
+        <div id="mail-subject"><a href="#">Nice to meet you!</a></div>
+        <div id="mail-time">3 hours ago</div>
+        <div id="star"><a href="#"><img src="/public/images/codeArmy/messages/star.png" /></a></div>
+        <div id="bin"><a href="#"><img src="/public/images/codeArmy/messages/bin.png" /></a></div>
+      </div>
+    </div>
+    <div id="inbox-top-bar">
+      <div id="back"><a href="#">Back</a></div>
+      <div id="mark-unread"><a href="#">Mark as unread</a></div>
+      <div id="star"><a href="#"><img src="/public/images/codeArmy/messages/star.png" /></a></div>
+      <div id="bin"><a href="#"><img src="/public/images/codeArmy/messages/bin.png" /></a></div>
+      <div id="msg-no">1 of 5</div>
+      <div id="arrow-left"><a href="#"><img src="/public/images/codeArmy/messages/arrow-left.png" /></a></div>
+      <div id="arrow-right"><a href="#"><img src="/public/images/codeArmy/messages/arrow-right.png" /></a></div>
+    </div>
+  </div>
+</div>
 <?php $this->load->view('includes/CAProfileFooter.php'); ?>
