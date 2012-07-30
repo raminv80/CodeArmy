0d6d3b1f4587a9b35b9ad78cc9a03f74fb292d3d
Ramin Vakilian <ramin@motionworks.com.my>
2012-07-30 10:48:37 +0800

    debug meesage inbox

--- public_html/application/models/message_model.php
+++ public_html/application/models/message_model.php
@@ -8,11 +8,11 @@ class Message_model extends CI_Model {
 	
 	function get_messages($user_id, $cat, $offset=-1, $limit=-1){
 		switch($cat){
-			case 'sent': $sql = "SELECT * FROM messages.*, f.username as from_username, t.username as to_username WHERE f.user_id=messages.from and t.user_id=messages.to and messages.from = ?";
+			case 'sent': $sql = "SELECT messages.*,f.username as from_username, t.username as to_username FROM messages, users as f, users as t WHERE f.user_id=messages.from and t.user_id=messages.to and messages.from = ?";
 						 if($offset>-1 && $limit>-1) $sql.=" limit ?,?";
 						 $data = $this->db->query($sql, array($user_id, $offset, $limit));
 			break;
-			case 'inbox': $sql = "SELECT * FROM messages.*, f.username as from_username, t.username as to_username WHERE f.user_id=messages.from and t.user_id=messages.to and messages.category IN ('inbox','important') AND messages.to = ?";
+			case 'inbox': $sql = "SELECT messages.*, f.username as from_username, t.username as to_username FROM messages, users as f, users as t WHERE f.user_id=messages.from and t.user_id=messages.to and messages.category IN ('inbox','important') AND messages.to = ?";
 						  if($offset>-1 && $limit>-1) $sql.=" limit ?,?";
 						  $data = $this->db->query($sql, array($user_id, $offset, $limit));
 			break;
