2cd6bc85643764edfb41e911348b31a4902fbf8b
Reza <reza@motionworks.com.my>
2012-08-02 19:21:34 +0800

    recommend talent page done

--- public_html/application/views/recom_tal_codearmy_view.php
+++ public_html/application/views/recom_tal_codearmy_view.php
@@ -1,10 +1,328 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Untitled Document</title>
-</head>
+<link href="/public/css/CodeArmyV1/style.css" media="all" rel="stylesheet" type="text/css" />
 
-<body>
-</body>
-</html>
+<div class="recom-tal-container"> <a href="#"><img class="recom-page-nav-left" src="/public/images/codeArmy/mission/recom-page-left-nav.png" /></a> <a href="#"><img class="recom-page-nav-right" src="/public/images/codeArmy/mission/recom-page-right-nav.png" /></a>
+  <div class="recom-tal-inner">
+    <div class="title-top-bar">
+      <div class="recom-title">
+        <div class="recom-title-main">Recommended Contractors</div>
+        <div class="recom-title-text">Brief explanation about Recommended Contractors here.</div>
+      </div>
+      <div class="recom-matches">
+        <div class="recom-matches-number">106</div>
+        <div class="recom-matches-text">matches</div>
+      </div>
+    </div>
+    <div class="recom-boxes-container">
+      <div class="recom-box-unit">
+        <div class="recom-box-top-left">
+          <div class="recom-box-top-circle">
+            <p>94%</p>
+          </div>
+          <div class="recom-box-name">John Doe</div>
+          <div class="recom-box-position">Web Design, Malaysia</div>
+          <div class="recom-box-badges"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"></div>
+        </div>
+        <div class="recom-box-avatar">
+          <p>Lvl. 85</p>
+        </div>
+        <div class="recom-box-desc">Short description about him/herself.</div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">PHP</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Photoshop</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Javascript</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Communication</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-invite-row">
+          <div class="recom-box-complete-missions">
+            <div class="recom-box-mis-com-txt">Mission Complete</div>
+            <div class="recom-box-mis-no">44</div>
+          </div>
+          <div class="recom-box-invite-button">
+            <input type="submit" value="Invite" class="lnkimg">
+          </div>
+        </div>
+      </div>
+      <div class="recom-box-unit">
+        <div class="recom-box-top-left">
+          <div class="recom-box-top-circle">
+            <p>94%</p>
+          </div>
+          <div class="recom-box-name">John Doe</div>
+          <div class="recom-box-position">Web Design, Malaysia</div>
+          <div class="recom-box-badges"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"></div>
+        </div>
+        <div class="recom-box-avatar">
+          <p>Lvl. 85</p>
+        </div>
+        <div class="recom-box-desc">Short description about him/herself.</div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">PHP</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Photoshop</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Javascript</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Communication</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-invite-row">
+          <div class="recom-box-complete-missions">
+            <div class="recom-box-mis-com-txt">Mission Complete</div>
+            <div class="recom-box-mis-no">44</div>
+          </div>
+          <div class="recom-box-invite-button">
+            <input type="submit" value="Invite" class="lnkimg">
+          </div>
+        </div>
+      </div>
+      <div class="recom-box-unit">
+        <div class="recom-box-top-left">
+          <div class="recom-box-top-circle">
+            <p>94%</p>
+          </div>
+          <div class="recom-box-name">John Doe</div>
+          <div class="recom-box-position">Web Design, Malaysia</div>
+          <div class="recom-box-badges"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"></div>
+        </div>
+        <div class="recom-box-avatar">
+          <p>Lvl. 85</p>
+        </div>
+        <div class="recom-box-desc">Short description about him/herself.</div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">PHP</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Photoshop</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Javascript</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Communication</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-invite-row">
+          <div class="recom-box-complete-missions">
+            <div class="recom-box-mis-com-txt">Mission Complete</div>
+            <div class="recom-box-mis-no">44</div>
+          </div>
+          <div class="recom-box-invite-button">
+            <input type="submit" value="Invite" class="lnkimg">
+          </div>
+        </div>
+      </div>
+      <div class="recom-box-unit">
+        <div class="recom-box-top-left">
+          <div class="recom-box-top-circle">
+            <p>94%</p>
+          </div>
+          <div class="recom-box-name">John Doe</div>
+          <div class="recom-box-position">Web Design, Malaysia</div>
+          <div class="recom-box-badges"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"></div>
+        </div>
+        <div class="recom-box-avatar">
+          <p>Lvl. 85</p>
+        </div>
+        <div class="recom-box-desc">Short description about him/herself.</div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">PHP</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Photoshop</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Javascript</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Communication</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-invite-row">
+          <div class="recom-box-complete-missions">
+            <div class="recom-box-mis-com-txt">Mission Complete</div>
+            <div class="recom-box-mis-no">44</div>
+          </div>
+          <div class="recom-box-invite-button">
+            <input type="submit" value="Invite" class="lnkimg">
+          </div>
+        </div>
+      </div>
+      <div class="recom-box-unit">
+        <div class="recom-box-top-left">
+          <div class="recom-box-top-circle">
+            <p>94%</p>
+          </div>
+          <div class="recom-box-name">John Doe</div>
+          <div class="recom-box-position">Web Design, Malaysia</div>
+          <div class="recom-box-badges"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"></div>
+        </div>
+        <div class="recom-box-avatar">
+          <p>Lvl. 85</p>
+        </div>
+        <div class="recom-box-desc">Short description about him/herself.</div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">PHP</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Photoshop</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Javascript</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Communication</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-invite-row">
+          <div class="recom-box-complete-missions">
+            <div class="recom-box-mis-com-txt">Mission Complete</div>
+            <div class="recom-box-mis-no">44</div>
+          </div>
+          <div class="recom-box-invite-button">
+            <input type="submit" value="Invite" class="lnkimg">
+          </div>
+        </div>
+      </div>
+      <div class="recom-box-unit">
+        <div class="recom-box-top-left">
+          <div class="recom-box-top-circle">
+            <p>94%</p>
+          </div>
+          <div class="recom-box-name">John Doe</div>
+          <div class="recom-box-position">Web Design, Malaysia</div>
+          <div class="recom-box-badges"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"><img src="/public/images/codeArmy/mission/recom-badge.png"></div>
+        </div>
+        <div class="recom-box-avatar">
+          <p>Lvl. 85</p>
+        </div>
+        <div class="recom-box-desc">Short description about him/herself.</div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">PHP</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Photoshop</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-prog-row">
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Javascript</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+          <div class="recom-box-prog-unit">
+            <div class="recom-box-prog-name">Communication</div>
+            <div class="recom-box-prog-bar">
+              <div class="recom-box-prog-inner"></div>
+            </div>
+          </div>
+        </div>
+        <div class="recom-box-invite-row">
+          <div class="recom-box-complete-missions">
+            <div class="recom-box-mis-com-txt">Mission Complete</div>
+            <div class="recom-box-mis-no">44</div>
+          </div>
+          <div class="recom-box-invite-button">
+            <input type="submit" value="Invite" class="lnkimg">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="recom-page-nav">jquery navigation</div>
+    <div class="recom-page-confirm-invite">
+      <input type="submit" value="Confirm Invite" class="lnkimg">
+    </div>
+  </div>
+</div>
