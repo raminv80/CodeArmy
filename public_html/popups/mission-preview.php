4ebd10057496850f8ef2da66a9fa4fd250bf4d4d
Reza <reza@motionworks.com.my>
2012-08-03 18:24:14 +0800

    find mission prog

---
+++ public_html/popups/mission-preview.php
@@ -0,0 +1,28 @@
+<link href="/public/css/CodeArmyV1/style.css" media="all" rel="stylesheet" type="text/css" />
+
+<div class="find-mission-full-container">
+  <div class="find-mission-main-col">
+    <div class="find_mission_main_desc">
+      <div class="find-mission-main-title">
+        <div class="find-mission-title-logo"></div>
+        <div class="find-mission-title-text">C# Framework for SME Site</div>
+      </div>
+      <div class="find-mission-video-block">
+        <iframe width="475" height="280" frameborder="0" src="http://www.youtube.com/embed/zFNb8j3YAd4?wmode=opaque" type="text/html" class="youtube-player"></iframe>
+      </div>
+      <div class="find-mission-main-desc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida feugiat feugiat. Duis adipiscing lorem a risus ullamcorper sed rhoncus risus fermentum. Suspendisse potenti. Proin eu arcu eu lectus imperdiet aliquet. Etiam egestas, lorem eu suscipit scelerisque, lacus justo bibendum odio, in lobortis ipsum diam gravida ante. <br />
+        <br />
+        Integer pellentesque rhoncus erat sit amet imperdiet. Aenean blandit rutrum leo, eget tincidunt justo adipiscing a. </div>
+    </div>
+    <div class="find-mission-comment-box">
+      <div class="find-mission-comment-box-left">
+        <div class="find-mission-comment-avatar"><img src="/public/images/codeArmy/mission/default-avatar.png" />
+          <p>75</p>
+        </div>
+        <div class="find-mission-comment-name">Rolando</div>
+      </div>
+      <div class="find-mission-comment-box-right"></div>
+    </div>
+  </div>
+  <div class="find-mission-right-col"> </div>
+</div>
