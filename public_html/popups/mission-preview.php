60f0339cee2b624ada82bbca2ccb367f2e77c04d
Reza <reza@motionworks.com.my>
2012-08-06 17:50:49 +0800

    find mission in progress

--- public_html/popups/mission-preview.php
+++ public_html/popups/mission-preview.php
@@ -21,7 +21,72 @@
         </div>
         <div class="find-mission-comment-name">Rolando</div>
       </div>
-      <div class="find-mission-comment-box-right"></div>
+      <div class="find-mission-comment-box-right">
+        <div class="fmcb-right-row1">
+          <div class="urbid">Your Bid</div>
+          <div class="clock-icon"></div>
+          <div class="small-box-hr">
+            <input type="text" value="18" />
+          </div>
+          <div class="botharrows"><a href="#"><img class="arr-up-img" src="/public/images/codeArmy/mission/arr-up.png" /></a><a href="#"><img class="arr-down-img" src="/public/images/codeArmy/mission/arr-down.png" /></a></div>
+          <div class="bidhrstext">Hours</div>
+          <div class="small-box-hr">
+            <input type="text" value="4" />
+          </div>
+          <div class="botharrows"><a href="#"><img class="arr-up-img" src="/public/images/codeArmy/mission/arr-up.png" /></a><a href="#"><img class="arr-down-img" src="/public/images/codeArmy/mission/arr-down.png" /></a></div>
+          <div class="bidhrstext">Days</div>
+        </div>
+        <div class="fmcb-right-row2">
+          <div class="dollar-sign-icon"></div>
+          <div class="small-box-hr">
+            <input type="text" value="35" />
+          </div>
+          <div class="bidhrstext">/hour</div>
+        </div>
+        <div class="fmcb-right-textarea">
+          <textarea rows="3">Ask a question</textarea>
+        </div>
+        <input type="submit" value="Submit" class="lnkimg" />
+      </div>
+      
+      <div class="find-mission-comment-box-down">
+        <div class="find-mission-down-box-row">
+           <div class="find-mission-comment-box-left">
+        <div class="find-mission-comment-avatar"><img src="/public/images/codeArmy/mission/default-avatar.png" />
+          <p>75</p>
+        </div>
+        <div class="find-mission-comment-name">Rolando</div>
+      </div>
+        
+        <div class="find-mission-comment-box-right">
+        <div class="fmcb-right-row1">
+          <div class="urbid">Your Bid</div>
+          <div class="clock-icon"></div>
+          <div class="small-box-hr">
+            <input type="text" value="18" />
+          </div>
+          <div class="botharrows"><a href="#"><img class="arr-up-img" src="/public/images/codeArmy/mission/arr-up.png" /></a><a href="#"><img class="arr-down-img" src="/public/images/codeArmy/mission/arr-down.png" /></a></div>
+          <div class="bidhrstext">Hours</div>
+          <div class="small-box-hr">
+            <input type="text" value="4" />
+          </div>
+          <div class="botharrows"><a href="#"><img class="arr-up-img" src="/public/images/codeArmy/mission/arr-up.png" /></a><a href="#"><img class="arr-down-img" src="/public/images/codeArmy/mission/arr-down.png" /></a></div>
+          <div class="bidhrstext">Days</div>
+        </div>
+        <div class="fmcb-right-row2">
+          <div class="dollar-sign-icon"></div>
+          <div class="small-box-hr">
+            <input type="text" value="35" />
+          </div>
+          <div class="bidhrstext">/hour</div>
+        </div>
+        
+      </div>
+        
+        </div>
+      </div>
+      
+      
     </div>
   </div>
   <div class="find-mission-right-col"> </div>
