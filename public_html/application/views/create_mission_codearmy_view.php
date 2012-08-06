caf33efcf9013cd36d3aac2d7ddfc0b573f186e8
mohdhazwan <hello@mohdhazwan.com>
2012-08-06 14:00:41 +0800

    Auto tagging script

--- public_html/application/views/create_mission_codearmy_view.php
+++ public_html/application/views/create_mission_codearmy_view.php
@@ -8,7 +8,7 @@
     </div>
     <div class="mission-video-url">
       <label>Mission Video</label>
-      <input type="text" id="mission-video" name="mission-video" value="" />
+      <input type="text" id="mission-video" name="mission-video" value="" placeholder="Video url"/>
     </div>
   </div>
   <div class="mission-desc-video">
@@ -23,7 +23,7 @@
       </div>
     </div>
     <div class="mission-video-preview">
-      <!--<iframe class="youtube-player" type="text/html" width="330" height="216" src="http://www.youtube.com/embed/zFNb8j3YAd4?wmode=opaque" frameborder="0"></iframe>-->
+    <iframe class="youtube-player" type="text/html" width="330" height="216" frameborder="0"></iframe>
     </div>
   </div>
   <div class="mission-type-n-skills">
@@ -48,7 +48,15 @@
       </select>
     </div>
     <div class="skills-required"> <span class="skills-required-title">Skills Required</span>
-      <textarea rows="7" id="skills-required-text" name="skills-required-text"></textarea>
+      	<!-- <textarea rows="3" id="skills-required-text" name="skills-required-text" contenteditable="true"></textarea> -->
+		<div id="skills-required-text" contenteditable="true"></div>
+		<div class="clearfix"></div>
+		<div class="skill-tag">
+			<a href="#" title="Web Design">Web Design</a>
+			<a href="#" title="CSS3">CSS3</a>
+			<a href="#" title="HTML5">HTML5</a>
+		</div>
+		<div class="skill-msg">Type the name of someone or something...</div>
     </div>
   </div>
   <div class="mission-arrange-budget">
@@ -90,3 +98,93 @@
   
 </div>
 </form>
+<!-- Temporary -->
+<style type="text/css">
+	.clearfix:before, .clearfix:after { content: "\0020"; display: block; height: 0; overflow: hidden; }  
+	.clearfix:after { clear: both; }  
+	.clearfix { zoom: 1; }
+	#skills-required-text {height:100px; background: url("/public/images/codeArmy/mission/textarea-box-bg.jpg") no-repeat; margin-left:40px; padding:5px; color:white}
+	#skills-required-text:focus {outline:none}
+	#skills-required-text a {padding:3px; background: black; color:#FC0}
+	#create-mission-container {overflow-x:hidden; overflow-y:auto}
+	.skill-tag {width:290px; padding:5px; background:black; margin-left:40px; display:none; }
+	.skill-tag a {color:white; width:100%; font-size:.85em; padding:3px; background: #333}
+	.skill-tag a:hover {background:#FC0; color:black}
+	.skill-msg {margin-left:40px; color: #FC0; display:none; font-size:.85em}
+</style>
+<script type="text/javascript">
+	$(function(){
+		//$('.fancybox-inner').css( {'overflow-x' : 'hidden','overflow-y' : 'auto'});
+		
+		var start=/@/ig;
+		var word=/@(\w+)/ig;
+		
+		$("#skills-required-text").live("keyup",function() {
+			var content = $(this).text();
+			var go = content.match(start);
+			var name= content.match(word);
+			var dataString = 'searchword='+ name;
+			
+			if (go){
+				$('.skill-msg, .skill-tag').slideDown('show');
+				if(name) {
+					$.ajax({
+						type: "POST",
+						url: "tagsearch.php",
+						data: dataString,
+						cache: false,
+						success: function(html) {
+							$(".skill-msg").hide();
+							$(".skill-tag").html(html).show();
+						}
+					});
+				};
+			};
+			return false();	
+		});
+		
+		$(".skill-tag a").live("click",function() 
+		{
+			var username = $(this).attr('title');
+			var old = $("#skills-required-text").html();
+			var content = old.replace(word,"");
+			$("#skills-required-text").html(content);
+			var E="<a contenteditable='false' href='#' >"+username+"</a>";
+			$("#skills-required-text").append(E+' ');
+			$(".skill-tag").hide();
+			$(".skill-msg").hide();
+			$("#skills-required-text").focus();
+		});
+		
+		$('#mission-video').live('keyup', function(){
+			var vidid = $(this).val();
+			var vidurl = vididParser(vidid);
+			console.log(vidurl);
+			
+			if (vidurl) {
+				var link = 'http://www.youtube.com/embed/'+vidurl+'?wmode=opaque';
+				$('.mission-video-preview').find('iframe').show().attr('src', link);
+			} else {
+				$('.mission-video-preview').find('iframe').hide();
+			}
+			/* if (vidid!=0){
+				var link = 'http://www.youtube.com/embed/'+vidid+'?wmode=opaque';
+				$('.mission-video-preview').find('iframe').attr('src', link);
+			} else {
+				$('.mission-video-preview').find('iframe').hide();
+			} */
+		})
+	});
+	function vididParser(url) {
+		if ('#mission-video:contains(vimeo)') {
+			var p = /http:\/\/(www\.)?vimeo.com\/(\d+)($|\/)/;
+		  	//return (url.match(p)) ? RegExp.$1 : false;
+			return (url.match(p));
+		} else if('#mission-video:contains(youtube)'){
+			var p = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?(?=.*v=((\w|-){11}))(?:\S+)?$/;
+			return (url.match(p));
+		} else {
+			
+		}
+	}
+</script>
