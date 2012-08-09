0d5a20c03e75f4d7bcef9002a6006501735b16e9
mohdhazwan <hello@mohdhazwan.com>
2012-08-09 17:46:30 +0800

    scrollbar applied to confirm_mission page

--- public_html/application/views/confirm_mission_codearmy_view.php
+++ public_html/application/views/confirm_mission_codearmy_view.php
@@ -1,65 +1,67 @@
 <?php $this->load->view('includes/frame_header.php'); ?>
 
 <?php echo form_open('missions/create_complete' , array('id'=>'form-create-mission')); ?>
-<div class="confirm-mission-container">
-  <div class="create-mission-title">Mission Preview</div>
-  <div class="confirm-mission-project-name">Project Name</div>
-  <div class="mission-brief">
-    <div class="mission-brief-title"><?=$preview['title']?></div>
-    <div class="brief-video">
-        <iframe width="330" height="216" frameborder="0" src="http://www.youtube.com/embed/<?=$preview['tutorial']?$preview['tutorial']:'zFNb8j3YAd4'?>?wmode=opaque" type="text/html" class="youtube-player"></iframe>
-    </div>
-    <div class="brief-text">
-      <div class="brief-text-title">Mission Briefing</div>
-      <div class="brief-text-content"><?=$preview['description']?></div>
-    </div>
-  </div>
-  <div class="mission-type-arrange-budget">
-    <div class="confirm-mission-type">
-      <div class="confirm-mission-type-title">Mission Type</div>
-      <div class="confirm-mission-type-text"><?=$preview['catname']?> > <?=$preview['classname']?> <?=$preview['subclassname']?></div>
-    </div>
-    <div class="confirm-mission-arrange-budget">
-      <div class="confirm-mission-arrange">
-        <div class="confirm-mission-type-title">Mission Arrangement</div>
-        <div class="confirm-mission-type-text"><?php if($arrangement != "") echo $arrangement['type']; else echo "Not Set";?> > <?php if($duration != "") echo $duration['duration']; else echo "Not Set";?></div>
-      </div>
-      <div class="confirm-mission-budget">
-        <div class="confirm-mission-type-title">Budget</div>
-        <div class="confirm-mission-type-text"><?php if($budget != "") echo $budget['amount']; else echo "Not Set";?></div>
-      </div>
-    </div>
-  </div>
-  <div class="confirm-mission-skills-files ">
-    <div class="confirm-mission-skills">
-      <div class="confirm-mission-skills-title">Skills Needed</div>
-      <div class="confirm-mission-skills-text"><?php
-	  foreach ($preview_skills as $i=>$skills):
-	  	echo $skills['skill_level']." ".$skills['name']."<br>";
-	  endforeach;
-      ?></div>
-    </div>
-    <div class="confirm-mission-files">
-      <div class="confirm-mission-files-title">Files Included</div>
-      <?php foreach($preview_files as $i=>$files): ?>
-      <div class="confirm-mission-files-text">
-        <div class="attach-file-tools"><img src="/public/images/codeArmy/mission/fileicon.png" class="fileicon" /> <span class="filename"><?=$files["file_name"]?></span></div>
-      </div>
-      <?php endforeach; ?>
-    </div>
-  </div>
-  <div class="confirm-mission-assign-po">
-    <!--<input type="checkbox" />
-    <label>Assigning this mission tp a Project Manager.</label>-->
-  </div>
-  <div class="submit-cancel-row"><input type="hidden" name="work_id" id="work_id" value="<?=$preview['work_id']?>" />
-    <input type="button" class="lnkimg" id="edit-mission" value="Edit">
-    <input type="button" class="lnkimg" id="confirm-mission" value="Confirm &amp; Upload">
-  </div>
-</div>
-</form>
-<div id="dialog-mission-creatd" class="dialog" title="Mission Created">
-	<p><span class="ui-icon ui-icon-check" style="float:left; margin:0 7px 20px 0;"></span>Your mission is successfully created.</p>
+<div class="create-mission-wrapper">
+	<div class="confirm-mission-container">
+	  <div class="create-mission-title">Mission Preview</div>
+	  <div class="confirm-mission-project-name">Project Name</div>
+	  <div class="mission-brief">
+	    <div class="mission-brief-title"><?=$preview['title']?></div>
+	    <div class="brief-video">
+	        <iframe width="330" height="216" frameborder="0" src="http://www.youtube.com/embed/<?=$preview['tutorial']?$preview['tutorial']:'zFNb8j3YAd4'?>?wmode=opaque" type="text/html" class="youtube-player"></iframe>
+	    </div>
+	    <div class="brief-text">
+	      <div class="brief-text-title">Mission Briefing</div>
+	      <div class="brief-text-content"><?=$preview['description']?></div>
+	    </div>
+	  </div>
+	  <div class="mission-type-arrange-budget">
+	    <div class="confirm-mission-type">
+	      <div class="confirm-mission-type-title">Mission Type</div>
+	      <div class="confirm-mission-type-text"><?=$preview['catname']?> > <?=$preview['classname']?> <?=$preview['subclassname']?></div>
+	    </div>
+	    <div class="confirm-mission-arrange-budget">
+	      <div class="confirm-mission-arrange">
+	        <div class="confirm-mission-type-title">Mission Arrangement</div>
+	        <div class="confirm-mission-type-text"><?php if($arrangement != "") echo $arrangement['type']; else echo "Not Set";?> > <?php if($duration != "") echo $duration['duration']; else echo "Not Set";?></div>
+	      </div>
+	      <div class="confirm-mission-budget">
+	        <div class="confirm-mission-type-title">Budget</div>
+	        <div class="confirm-mission-type-text"><?php if($budget != "") echo $budget['amount']; else echo "Not Set";?></div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="confirm-mission-skills-files ">
+	    <div class="confirm-mission-skills">
+	      <div class="confirm-mission-skills-title">Skills Needed</div>
+	      <div class="confirm-mission-skills-text"><?php
+		  foreach ($preview_skills as $i=>$skills):
+		  	echo $skills['skill_level']." ".$skills['name']."<br>";
+		  endforeach;
+	      ?></div>
+	    </div>
+	    <div class="confirm-mission-files">
+	      <div class="confirm-mission-files-title">Files Included</div>
+	      <?php foreach($preview_files as $i=>$files): ?>
+	      <div class="confirm-mission-files-text">
+	        <div class="attach-file-tools"><img src="/public/images/codeArmy/mission/fileicon.png" class="fileicon" /> <span class="filename"><?=$files["file_name"]?></span></div>
+	      </div>
+	      <?php endforeach; ?>
+	    </div>
+	  </div>
+	  <div class="confirm-mission-assign-po">
+	    <!--<input type="checkbox" />
+	    <label>Assigning this mission tp a Project Manager.</label>-->
+	  </div>
+	  <div class="submit-cancel-row"><input type="hidden" name="work_id" id="work_id" value="<?=$preview['work_id']?>" />
+	    <input type="button" class="lnkimg" id="edit-mission" value="Edit">
+	    <input type="button" class="lnkimg" id="confirm-mission" value="Confirm &amp; Upload">
+	  </div>
+	</div>
+	</form>
+	<div id="dialog-mission-creatd" class="dialog" title="Mission Created">
+		<p><span class="ui-icon ui-icon-check" style="float:left; margin:0 7px 20px 0;"></span>Your mission is successfully created.</p>
+	</div>
 </div>
 
 <script type="text/javascript">
@@ -130,5 +132,44 @@ $('#confirm-mission').click(function(){
 		}
 	);
 });
+
+var win = $(window);
+// Full body scroll
+var isResizing = false;
+win.bind(
+	'resize',
+	function()
+	{
+		if (!isResizing) {
+			isResizing = true;
+			var container = $('.create-mission-wrapper');
+			// Temporarily make the container tiny so it doesn't influence the
+			// calculation of the size of the document
+			container.css(
+				{
+					'width': 1,
+					'height': 1
+				}
+			);
+			// Now make it the size of the window...
+			container.css(
+				{
+					'width': win.width(),
+					'height': win.height()
+				}
+			);
+			isResizing = false;
+			container.jScrollPane(
+				{
+					'showArrows': true
+				}
+			);
+		}
+	}
+).trigger('resize');
+/* IE calculates the width incorrectly first time round (it doesn't count the space used by the native scrollbar) so we re-trigger if necessary. */
+if ($('#full-page-container').width() != win.width()) {
+	win.trigger('resize');
+}
 </script>
 <?php $this->load->view('includes/frame_footer.php'); ?>
