8b5bd4f808db5e07516bf23d63ce8e3d9dce63d7
Reza <reza@motionworks.com.my>
2012-08-10 12:51:32 +0800

    mission conflict

--- public_html/application/controllers/missions.php
+++ public_html/application/controllers/missions.php
@@ -434,8 +434,4 @@ class Missions extends CI_Controller {
 		endforeach;
 		//echo "<a href=\"#\">hello world</a>";
 	}
-	function wall(){
-		$this->view_data['window_title'] = "CodeArmy Wall";
-		$this->load->view('wall_codearmy_view', $this->view_data);
-	}
 }
