bf59697d7e7d3e12f808ae49627ba612171cafd8
mohdhazwan <hello@mohdhazwan.com>
2012-08-16 15:30:25 +0800

    faq scrollbar

--- public_html/application/views/faq_codearmy_view.php
+++ public_html/application/views/faq_codearmy_view.php
@@ -1,116 +1,67 @@
 <?php $this->load->view('includes/CAProfileHeader.php'); ?>
 <style>
-.faqwrapper {
-	border: 1px solid #999999;
+.faqcontainer .faqwrapper {
 	height: 800px;
 	overflow: auto;
 	padding: 10px;
 	line-height:1.1;
 }
-h3 {
+.faqcontainer h3 {
 	font-size:18px;
 	font-weight:bold;
 }
-h4 {
+.faqcontainer h4 {
 	font-size:14px;
 	font-weight:bold;
+	margin-bottom:5px;
 }
+
+.faqcontainer .faqwrapper p {margin-bottom:20px}
 </style>
-<h3>FAQ for Code Ar.my</h3>
-<img src="/public/images/codeArmy/content/title-bg.png" id="preview"/> <br />
-<br />
-<br />
-<br />
-<div class="faqwrapper">
-  <h4>1. What is CodeAr.my ?</h4>
-  <br />
-  <br />
-  CodeAr.my is an interactive platform that combines online employment opportunities with a gaming experience, to allow users to freely bid and display their abilities with skill points and reward badges in order to build their online work portfolio. <br />
-  <br />
-  <br />
-  <h4>2. Why is Code Ar.my spelled that way?</h4>
-  <br />
-  <br />
-  Coined with a '.my', CodeAr.my signifies the unrelenting commitment to Malaysia and to firmly rooting the platform here (.my is the country code for Malaysia). First and foremost, it is an opportunity for the Malaysian talent base to get a step-up and interact on a global platform, and to work as an ongoing vehicle to help Malaysians become more prominent in this sector. With the project being rolled out for the benefit of the entire country, CodeAr.my has been involved in successful discussions with  MDec, in order to secure support for the evolution and activation of the platform. <br />
-  <br />
-  <br />
-  <h4>3. Why should I use Code Ar.my?</h4>
-  <br />
-  <br />
-  CodeAr.my allows you to start with tasks that are best suited to your current skill levels and build your expertise and knowledge through various micro training modules as you continue your growth. The best part is, every task you complete will be reflected on your CodeAr.my profile, constantly elevating you to higher levels. CodeAr.my gives you the freedom to choose and specialise in skills that you prefer. Don't forget, through CodeAr.my you start to earn your MONEY doing what you love and working when you wish! <br />
-  <br />
-  <br />
-  <h4>4. How does Code Ar.my work ?</h4>
-  <br />
-  <br />
-  The general concept that Code Ar.my operates on is by bidding via crowdsourcing or trial sourcing. This is best explained by a project owner that uploads a job description that provides freelancers with information on the task.  The information on the task will include the background, expectations, job priority level, job difficulty and budget for the job. <br />
-  Freelancers will then bid for the job by offering their offer to take the job and complete it with within a certain length of time.  Upon job completion the project owner will transfer the fee to the freelancers using an escrow service to compensate freelancers. <br />
-  <br />
-  <br />
-  <h4>5. How do I bid for jobs? - Amount vs. Pays - Rate of difficulty</h4>
-  <br />
-  <br />
-  First you need to be logged in to system. Go to browse jobs and select/expand your desired job to view its details then click on "bid & more" button at right side of job details panel to access the job profile. Job profile provides complete description of task. At bottom of the page you can see the bidding widget. The bidding allows players to declare their desired amount and day to complete a task. Once your bid is set click on bid button to place your bid. If your bid is successful you will be notified by system through email. Pricing of your bid depends on what you feel is the fair market rate for the task undertaken. CodeAr.my does not have a standard rate card, and we view pricing as a mutual agreement between the project owner and the talent / freelancer <br />
-  <br />
-  <br />
-  <h4>6. How do I add skills? - Skill level correlation to main, second and third</h4>
-  <br />
-  <br />
-  Users are able to add 3 unique skills that consists of 'hard or soft skills' to his/her profile on the dashboard page of Code Ar.my. From then on, users are able to level-up based on experience points that are gained by finishing jobs that they have completed, or via the self-development tasks. <br />
-  <br />
-  <br />
-  <h4>7. What is the relationship between my skills and bidding for jobs?</h4>
-  <br />
-  <br />
-  Each task has a list of skills and required level attached. This list might be used to filter out bidders based on their skills or to justify the amount of bid. In other words the chance of winning a bid will raise up by my level of skills. <br />
-  <br />
-  <br />
-  <h4>8. How do I level up my skill points? -> Spending skill points to increase skill level</h4>
-  <br />
-  <br />
-  As you actively engage with CodeAr.my, you will be have the opportunity to earn points as you complete your tasks. These accumulated points can be traded in to buy skill points that allow you specialize in certain skills which either by completion of general task or specialized task that you have bidded for.  Building up on specialized skills gives you an edge of over other users of CodeAr.my as you have begun to specialize on certain skills other users may have not. <br />
-  <br />
-  <br />
-  <h4>9. How do I submit my job bid?</h4>
-  <br />
-  <br />
-  After registration with Code Ar.my you will be directed to your dashboard. This section will provide you the option that allows you to browse for jobs. After choosing your desired job, you will be directed to the profile page of the job. From the profile page you will be able to see a submit button to bid for the job, where you outline the various components of your bid for the project owner to evaluate. <br />
-  <br />
-  <br />
-  <h4>10. How do I download file attachment?</h4>
-  <br />
-  <br />
-  As a CodeAr.my user, there will be various times when your job may require you to download project owner's files in order for you to carry out your duties. In this situation where content has to be downloaded, the download button will be clickable. <br />
-  <br />
-  <br />
-  <h4>11. How do I upload my work? - Do I need to compress? - Upload format</h4>
-  <br />
-  <br />
-  There are three options to upload your work. The irst option is to use Github.com to upload you work to a repository and add its link. The second option is to upload project files as an archive into a server and a link to files. The third option is to archive files using zip format and upload them to CodeAr.my using the upload widget located at task submission page. <br />
-  <br />
-  <br />
-  <h4>12. Can I negotiate my hours once confirmed?</h4>
-  <br />
-  <br />
-  Yes you will be able to negotiate your job details by communicating with your project owner through the discussion board, which facilitates communication with your project owner. You will be able to negotiate aspects of your contract with your project owner either as a document or by a record your conversations. <br />
-  <br />
-  <br />
-  <h4>13. How many ranks and position is available?</h4>
-  <br />
-  <br />
-  There are over 100 ranks and positions available in CodeAr.my. When you register with CodeAr.my, you will be asked to verify your skills based upon a set of evaluation tasks. This will set your base rank, and as you complete tasks and jobs successfully for project owners, your ranking will improve. <br />
-  <br />
-  <br />
-  <h4>14. When is the full launch?</h4>
-  <br />
-  <br />
-  Code Ar.my Open Beta Launch is to be in September 2012 while complete full version tentatively scheduled to be in December 2013. <br />
-  <br />
-  <br />
-  <h4>15. If I'm not a programmer, can I still join the Ar.my?</h4>
-  <br />
-  <br />
-  Developing digital products is no longer only in the realm of programmers - there are so many other specialisations that are involved. CodeAr.my seeks people with the relevant skills to enable a project to be completed. So, if you have skills relevant to the creation of digital projects, then CodeAr.my is for you. <br />
-  <br />
+<div class="faqcontainer">
+	<h3>FAQ for Code Ar.my</h3>
+	<img src="/public/images/codeArmy/content/title-bg.png" id="preview"/>
+	<div class="faqwrapper">
+	<h4>1. What is CodeAr.my ?</h4>
+	<p>CodeAr.my is an interactive platform that combines online employment opportunities with a gaming experience, to allow users to freely bid and display their abilities with skill points and reward badges in order to build their online work portfolio.</p>
+	<h4>2. Why is Code Ar.my spelled that way?</h4>
+	<p>Coined with a '.my', CodeAr.my signifies the unrelenting commitment to Malaysia and to firmly rooting the platform here (.my is the country code for Malaysia). First and foremost, it is an opportunity for the Malaysian talent base to get a step-up and interact on a global platform, and to work as an ongoing vehicle to help Malaysians become more prominent in this sector. With the project being rolled out for the benefit of the entire country, CodeAr.my has been involved in successful discussions with  MDec, in order to secure support for the evolution and activation of the platform.</p>
+	<h4>3. Why should I use Code Ar.my?</h4>
+	<p>CodeAr.my allows you to start with tasks that are best suited to your current skill levels and build your expertise and knowledge through various micro training modules as you continue your growth. The best part is, every task you complete will be reflected on your CodeAr.my profile, constantly elevating you to higher levels. CodeAr.my gives you the freedom to choose and specialise in skills that you prefer. Don't forget, through CodeAr.my you start to earn your MONEY doing what you love and working when you wish!</p>
+	  <h4>4. How does Code Ar.my work ?</h4>
+	  <p>The general concept that Code Ar.my operates on is by bidding via crowdsourcing or trial sourcing. This is best explained by a project owner that uploads a job description that provides freelancers with information on the task.  The information on the task will include the background, expectations, job priority level, job difficulty and budget for the job.</p>
+	  <p>Freelancers will then bid for the job by offering their offer to take the job and complete it with within a certain length of time.  Upon job completion the project owner will transfer the fee to the freelancers using an escrow service to compensate freelancers.</p>
+	  <h4>5. How do I bid for jobs? - Amount vs. Pays - Rate of difficulty</h4>
+	  <p>First you need to be logged in to system. Go to browse jobs and select/expand your desired job to view its details then click on "bid & more" button at right side of job details panel to access the job profile. Job profile provides complete description of task. At bottom of the page you can see the bidding widget. The bidding allows players to declare their desired amount and day to complete a task. Once your bid is set click on bid button to place your bid. If your bid is successful you will be notified by system through email. Pricing of your bid depends on what you feel is the fair market rate for the task undertaken. CodeAr.my does not have a standard rate card, and we view pricing as a mutual agreement between the project owner and the talent / freelancer</p>
+	  <h4>6. How do I add skills? - Skill level correlation to main, second and third</h4>
+	  <p>Users are able to add 3 unique skills that consists of 'hard or soft skills' to his/her profile on the dashboard page of Code Ar.my. From then on, users are able to level-up based on experience points that are gained by finishing jobs that they have completed, or via the self-development tasks.</p>
+	  <h4>7. What is the relationship between my skills and bidding for jobs?</h4>
+	  <p>Each task has a list of skills and required level attached. This list might be used to filter out bidders based on their skills or to justify the amount of bid. In other words the chance of winning a bid will raise up by my level of skills.</p>
+	  <h4>8. How do I level up my skill points? -> Spending skill points to increase skill level</h4>
+	  <p>As you actively engage with CodeAr.my, you will be have the opportunity to earn points as you complete your tasks. These accumulated points can be traded in to buy skill points that allow you specialize in certain skills which either by completion of general task or specialized task that you have bidded for.  Building up on specialized skills gives you an edge of over other users of CodeAr.my as you have begun to specialize on certain skills other users may have not.</p>
+	  <h4>9. How do I submit my job bid?</h4>
+	  <p>After registration with Code Ar.my you will be directed to your dashboard. This section will provide you the option that allows you to browse for jobs. After choosing your desired job, you will be directed to the profile page of the job. From the profile page you will be able to see a submit button to bid for the job, where you outline the various components of your bid for the project owner to evaluate.</p>
+	  <h4>10. How do I download file attachment?</h4>
+	  <p>As a CodeAr.my user, there will be various times when your job may require you to download project owner's files in order for you to carry out your duties. In this situation where content has to be downloaded, the download button will be clickable.</p>
+	  <h4>11. How do I upload my work? - Do I need to compress? - Upload format</h4>
+	  <p>There are three options to upload your work. The irst option is to use Github.com to upload you work to a repository and add its link. The second option is to upload project files as an archive into a server and a link to files. The third option is to archive files using zip format and upload them to CodeAr.my using the upload widget located at task submission page.</p>
+	  <h4>12. Can I negotiate my hours once confirmed?</h4>
+	  <p>Yes you will be able to negotiate your job details by communicating with your project owner through the discussion board, which facilitates communication with your project owner. You will be able to negotiate aspects of your contract with your project owner either as a document or by a record your conversations.</p>
+	  <h4>13. How many ranks and position is available?</h4>
+	  <p>There are over 100 ranks and positions available in CodeAr.my. When you register with CodeAr.my, you will be asked to verify your skills based upon a set of evaluation tasks. This will set your base rank, and as you complete tasks and jobs successfully for project owners, your ranking will improve.</p> 
+	  <h4>14. When is the full launch?</h4>
+	  <p>Code Ar.my Open Beta Launch is to be in September 2012 while complete full version tentatively scheduled to be in December 2013.</p>
+	  <h4>15. If I'm not a programmer, can I still join the Ar.my?</h4>
+	  <p>Developing digital products is no longer only in the realm of programmers - there are so many other specialisations that are involved. CodeAr.my seeks people with the relevant skills to enable a project to be completed. So, if you have skills relevant to the creation of digital projects, then CodeAr.my is for you.</p>
+	</div>
 </div>
+
+<script type="text/javascript" src="/public/js/jscrollpane.js"></script>
+<script type="text/javascript" src="http://jscrollpane.kelvinluck.com/script/jquery.mousewheel.js"></script>
+<script type="text/javascript">
+	$(function(){
+		$('.faqwrapper').jScrollPane();
+	})
+</script>
+
 <?php $this->load->view('includes/CAProfileFooter.php'); ?>
