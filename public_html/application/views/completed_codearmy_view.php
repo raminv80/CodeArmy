a8525eeca4839ba62d528d5287b6d8b244fb5142
Reza <reza@motionworks.com.my>
2012-08-17 13:31:16 +0800

    completed mission page done

--- public_html/application/views/completed_codearmy_view.php
+++ public_html/application/views/completed_codearmy_view.php
@@ -1,3 +1,83 @@
 <?php $this->load->view('includes/CAProfileHeader.php'); ?>
-<img src="/public/images/codeArmy/profile/missions/temp-completed.png" id="preview"/>
+
+<div class="completed-mission-container">
+  <div class="block-header">
+    <h3>Completed Missions</h3>
+  </div>
+  <div class="completed-missions-table">
+    <table border="1" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><div class="row-no">1</div>
+          <div class="com-mission-info">
+            <div class="com-mission-title">Web design for Nike, completed in 30 hours.</div>
+            <div class="com-mission-date">06th May 2012</div>
+            <div class="com-mission-desc">Mission description:<br />
+              Created a new site for a single shoe that we're introducing into Australia. Site was flash and interactive and linked back to the main Nike site.</div>
+          </div></td>
+        <td><div class="com-mission-review">Review</div>
+          <div class="com-mission-from">From Commander</div>
+          <div class="com-mission-rev-text">"Nice Work"</div></td>
+        <td><div class="com-mission-pay-title">Payout Details</div>
+          <div class="com-mission-pay-method">Paypal</div>
+          <div class="com-mission-pay-amount">$ 400.00 for 10 hours</div></td>
+      </tr>
+      <tr>
+        <td><div class="row-no">2</div>
+          <div class="com-mission-info">
+            <div class="com-mission-title">Copywriting for Nike website, completed  in 5 hours.</div>
+            <div class="com-mission-date">01th May 2012</div>
+            <div class="com-mission-readmore"><a href="#">read more</a></div>
+          </div></td>
+        <td><div class="com-mission-review">Review</div>
+          <div class="com-mission-from">From Commander</div>
+          <div class="com-mission-rev-text">"Nice Work"</div></td>
+        <td><div class="com-mission-pay-title">Payout Details</div>
+          <div class="com-mission-pay-method">Paypal</div>
+          <div class="com-mission-pay-amount">$ 400.00 for 10 hours</div></td>
+      </tr>
+      <tr>
+        <td><div class="row-no">3</div>
+          <div class="com-mission-info">
+            <div class="com-mission-title">Copywriting for Nike website, completed  in 5 hours.</div>
+            <div class="com-mission-date">01th May 2012</div>
+            <div class="com-mission-readmore"><a href="#">read more</a></div>
+          </div></td>
+        <td><div class="com-mission-review">Review</div>
+          <div class="com-mission-from">From Commander</div>
+          <div class="com-mission-rev-text">"Nice Work"</div></td>
+        <td><div class="com-mission-pay-title">Payout Details</div>
+          <div class="com-mission-pay-method">Paypal</div>
+          <div class="com-mission-pay-amount">$ 400.00 for 10 hours</div></td>
+      </tr>
+      <tr>
+        <td><div class="row-no">4</div>
+          <div class="com-mission-info">
+            <div class="com-mission-title">Copywriting for Nike website, completed  in 5 hours.</div>
+            <div class="com-mission-date">01th May 2012</div>
+            <div class="com-mission-readmore"><a href="#">read more</a></div>
+          </div></td>
+        <td><div class="com-mission-review">Review</div>
+          <div class="com-mission-from">From Commander</div>
+          <div class="com-mission-rev-text">"Nice Work"</div></td>
+        <td><div class="com-mission-pay-title">Payout Details</div>
+          <div class="com-mission-pay-method">Paypal</div>
+          <div class="com-mission-pay-amount">$ 400.00 for 10 hours</div></td>
+      </tr>
+      <tr>
+        <td><div class="row-no">5</div>
+          <div class="com-mission-info">
+            <div class="com-mission-title">Copywriting for Nike website, completed  in 5 hours.</div>
+            <div class="com-mission-date">01th May 2012</div>
+            <div class="com-mission-readmore"><a href="#">read more</a></div>
+          </div></td>
+        <td><div class="com-mission-review">Review</div>
+          <div class="com-mission-from">From Commander</div>
+          <div class="com-mission-rev-text">"Nice Work"</div></td>
+        <td><div class="com-mission-pay-title">Payout Details</div>
+          <div class="com-mission-pay-method">Paypal</div>
+          <div class="com-mission-pay-amount">$ 400.00 for 10 hours</div></td>
+      </tr>
+    </table>
+  </div>
+</div>
 <?php $this->load->view('includes/CAProfileFooter.php'); ?>
