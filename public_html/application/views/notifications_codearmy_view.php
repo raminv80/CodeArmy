5cc7c07b510e7193808fc5fe65abf28a27abcd80
Reza <reza@motionworks.com.my>
2012-08-17 16:33:53 +0800

    notification page done

--- public_html/application/views/notifications_codearmy_view.php
+++ public_html/application/views/notifications_codearmy_view.php
@@ -1,3 +1,39 @@
 <?php $this->load->view('includes/CAProfileHeader.php'); ?>
-<img src="/public/images/codeArmy/messages/temp-notification.png" id="preview"/>
+
+
+<div class="notification-container">
+
+<div class="block-header">
+    <h3>Your Notifications</h3>
+  </div>
+  
+  <div class="notif-date">Today</div>
+  <div class="notif-text">
+  <ul>
+  <li>
+  Harish receives a mission bid <a href="#">confirmation</a>. 3:50pm
+  </li>
+  </ul>
+  </div>
+  
+  <div class="notif-date">Yesterday</div>
+  <div class="notif-text">
+  <ul>
+  <li>Jack posted in the <a href="#">discussion board</a> of mission: web designer ... 4:00pm</li>
+  <li>Captain replied in the <a href="#">discussion board.</a> 2:50pm</li>
+  <li>Calvin sent a <a href="#">message</a> to you.</li>
+  </ul>
+  </div>
+  
+  <div class="notif-date">12th May</div>
+  <div class="notif-text">
+  <ul>
+  <li>Captain replied in the <a href="#">discussion board.</a> 3:45pm</li>
+  </ul>
+  </div>
+  
+  
+
+</div>
+
 <?php $this->load->view('includes/CAProfileFooter.php'); ?>
