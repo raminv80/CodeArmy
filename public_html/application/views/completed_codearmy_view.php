dd0737aef6723794fa51126dbd4af9657b96720d
mohdhazwan <hello@mohdhazwan.com>
2012-09-18 17:44:12 +0800

    full page layout for - mission bid - mission completed

--- public_html/application/views/completed_codearmy_view.php
+++ public_html/application/views/completed_codearmy_view.php
@@ -1,34 +1,42 @@
 <?php $this->load->view('includes/CAProfileHeader.php'); ?>
 
-<div class="completed-mission-container">
-  <div class="block-header">
-    <h3>Completed Missions</h3>
-  </div>
-  <div class="completed-missions-table">
+<div class="container-fluid">
+	<div class="row-fluid">
+		
+		<!-- Page start -->
+		<div class="completed-mission-container span10 offset1">
+		  <div class="block-header">
+		    <h3>Completed Missions</h3>
+		  </div>
+		  <div class="completed-missions-table">
 
-    <table border="1" cellspacing="0" cellpadding="0">
-      <?php foreach($missions as $i=>$mission):
-	  	$work_id = $mission['work_id'];
-	  	$hours = $this->work_model->mission_completed_hours($work_id);
-		$arrangement_type = $this->work_model->get_work_arrangement($work_id);
-		$arrangement = $this->work_model->get_actual_work_time_cost($work_id);
-	  ?>
-      <tr>
-        <td><div class="row-no"><?=$i+1?></div>
-          <div class="com-mission-info">
-            <div class="com-mission-title"><?=$mission['title']?></div>
-            <div class="com-mission-date">Done at <?=date('jth M Y',strtotime($mission['done_at']))?></div>
-            <div class="com-mission-desc"><a href="/missions/wall/<?=$mission['work_id']?>">Mission details...</a></div>
-          </div></td>
-        <td><div class="com-mission-review">Review</div>
-          <div class="com-mission-from">From Commander</div>
-          <div class="com-mission-rev-text">"Nice Work"</div></td>
-        <td><div class="com-mission-pay-title">Payout Details</div>
-          <div class="com-mission-pay-method">Manual</div>
-          <div class="com-mission-pay-amount">$ <?=$arrangement['bid_cost']*$arrangement['bid_time']?> for <?=$arrangement['bid_time']?> <?=str_replace('dai','day',substr($arrangement_type,0,-2))?>s</div></td>
-      </tr>
-      <?php endforeach;?>
-    </table>
-  </div>
+		    <table border="1" cellspacing="0" cellpadding="0">
+		      <?php foreach($missions as $i=>$mission):
+			  	$work_id = $mission['work_id'];
+			  	$hours = $this->work_model->mission_completed_hours($work_id);
+				$arrangement_type = $this->work_model->get_work_arrangement($work_id);
+				$arrangement = $this->work_model->get_actual_work_time_cost($work_id);
+			  ?>
+		      <tr>
+		        <td><div class="row-no"><?=$i+1?></div>
+		          <div class="com-mission-info">
+		            <div class="com-mission-title"><?=$mission['title']?></div>
+		            <div class="com-mission-date">Done at <?=date('jth M Y',strtotime($mission['done_at']))?></div>
+		            <div class="com-mission-desc"><a href="/missions/wall/<?=$mission['work_id']?>">Mission details...</a></div>
+		          </div></td>
+		        <td><div class="com-mission-review">Review</div>
+		          <div class="com-mission-from">From Commander</div>
+		          <div class="com-mission-rev-text">"Nice Work"</div></td>
+		        <td><div class="com-mission-pay-title">Payout Details</div>
+		          <div class="com-mission-pay-method">Manual</div>
+		          <div class="com-mission-pay-amount">$ <?=$arrangement['bid_cost']*$arrangement['bid_time']?> for <?=$arrangement['bid_time']?> <?=str_replace('dai','day',substr($arrangement_type,0,-2))?>s</div></td>
+		      </tr>
+		      <?php endforeach;?>
+		    </table>
+		  </div>
+		</div>
+		<!-- Page ends -->
+		
+	</div>
 </div>
 <?php $this->load->view('includes/CAProfileFooter.php'); ?>
--- public_html/application/views/mybids_codearmy_view.php
+++ public_html/application/views/mybids_codearmy_view.php
@@ -1,63 +1,72 @@
 <?php $this->load->view('includes/CAProfileHeader.php'); ?>
 
-<div id="mybids-content-area"> 
-  
-  <!-- START - Mission Bid Title Block - Dev. by Reza  -->
-  <div id="block-mission-bid-header">
-    <div class="block-header">
-      <h3>Mission Bids</h3>
-    </div>
-  </div>
-  
-  <!-- START - Mission Bid Info Blocks - Dev. by Reza  -->
-  <?php 
-	$cur_date = "";
-	foreach($bids as $bid):
-	$bid_date = date('j M Y',strtotime($bid['created_at']));
-  ?>
-  <div id="block-bid-container" <?php if($bid['bid_status']=='Accepted'){?>class="latest-bid-container"<?php }?>>
-  	<?php if($bid_date!=$cur_date){$cur_date=$bid_date;?>
-    <div id="bid-date"><?=$bid_date;?></div>
-    <?php }?>
-    <div id="bid-info-block">
-      <div id="bid-info-block-left" style="cursor:pointer" onclick="view_mission('<?=$bid['work_id']?>')">
-        <div id="bid-close-date"><?php if($bid['bid_deadline']){?>Bid closes on <?php echo date('j M Y',strtotime($bid['bid_deadline'])); }?></div>
-        <div id="proj-title"><?=$bid['title']?></div>
-      </div>
-      <div id="bid-info-block-right">
-        <div id="highest-bid">Your final bid</div>
-        <div id="time-price">
-          <div id="bid-time"><?=$bid['bid_time']?> <?=str_replace('dai','day',substr($bid['arrangement'],0,-2))?>s</div>
-          <div id="bid-price">$ <?=$bid['bid_cost']?></div>
-        </div>
-      </div>
-    </div>
-    <div id="bid-msg">
-      <div id="bid-msg-text">
-      	<?php if($bid['bid_status']=='Accepted' && $bid['status']=='assigned'){//if i won and mission is just assigned to me?>
-        <p><a href="#">Report to the captain</a></p>
-        <?php }elseif($bid['bid_status']=='Accepted' && in_array(strtolower($bid['status']),array('in progress','done','redo','verify','signoff'))){//if i won and mission is in progress?>
-        <p>You won this bid on <?=date('j M Y',strtotime($bid['assigned_at']))?></p>
-        <?php }elseif($bid['bid_status']=='Bid' && !in_array(strtolower($bid['status']),array('open','reject'))){//if i lost the biod?>
-        <p>Sorry! you lost this bid. <a href="#">How to win a bid?</a></p>
-        <?php }elseif($bid['bid_status']=='Bid' && in_array(strtolower($bid['status']),array('open','reject'))){//if still bid is ongoing?>
-        <p>Average bids: </br><b style="color:#0FF"><?=round($bid['avg_time'])?></b> <?=str_replace('dai','day',substr($bid['arrangement'],0,-2))?>s for USD <b style="color:#0FF"><?=round($bid['avg_cost'])?></b>/<?=str_replace('dai','day',substr($bid['arrangement'],0,-2))?></p>
-        <?php }?>
-      </div>
-      <?php if(($bid['bid_status']=='Bid')||($bid['bid_status']=='Accepted' && in_array(strtolower($bid['status']),array('open','reject')))){?>
-      <div id="stop-following-bid"><a class="unwatch_bid" title="Cancels your bid and stops following on this mission" href="javascript:void(0)" id="unwatch_<?=$bid['bid_id']?>">Stop following</a></div>
-      <?php }?>
-    </div>
-  </div>
-  <?php endforeach;?>
+<div class="container-fluid">
+	<div class="row-fluid" style="padding-bottom:50px">
+		
+		<!-- Page start -->
+		<div id="mybids-content-area" class="span10 offset1"> 
+
+		  <!-- START - Mission Bid Title Block - Dev. by Reza  -->
+		  <div id="block-mission-bid-header">
+		    <div class="block-header">
+		      <h3>Mission Bids</h3>
+		    </div>
+		  </div>
+
+		  <!-- START - Mission Bid Info Blocks - Dev. by Reza  -->
+		  <?php 
+			$cur_date = "";
+			foreach($bids as $bid):
+			$bid_date = date('j M Y',strtotime($bid['created_at']));
+		  ?>
+		  <div id="block-bid-container" <?php if($bid['bid_status']=='Accepted'){?>class="latest-bid-container"<?php }?>>
+		  	<?php if($bid_date!=$cur_date){$cur_date=$bid_date;?>
+		    <div id="bid-date"><?=$bid_date;?></div>
+		    <?php }?>
+		    <div id="bid-info-block">
+		      <div id="bid-info-block-left" style="cursor:pointer" onclick="view_mission('<?=$bid['work_id']?>')">
+		        <div id="bid-close-date"><?php if($bid['bid_deadline']){?>Bid closes on <?php echo date('j M Y',strtotime($bid['bid_deadline'])); }?></div>
+		        <div id="proj-title"><?=$bid['title']?></div>
+		      </div>
+		      <div id="bid-info-block-right">
+		        <div id="highest-bid">Your final bid</div>
+		        <div id="time-price">
+		          <div id="bid-time"><?=$bid['bid_time']?> <?=str_replace('dai','day',substr($bid['arrangement'],0,-2))?>s</div>
+		          <div id="bid-price">$ <?=$bid['bid_cost']?></div>
+		        </div>
+		      </div>
+		    </div>
+		    <div id="bid-msg">
+		      <div id="bid-msg-text">
+		      	<?php if($bid['bid_status']=='Accepted' && $bid['status']=='assigned'){//if i won and mission is just assigned to me?>
+		        <p><a href="#">Report to the captain</a></p>
+		        <?php }elseif($bid['bid_status']=='Accepted' && in_array(strtolower($bid['status']),array('in progress','done','redo','verify','signoff'))){//if i won and mission is in progress?>
+		        <p>You won this bid on <?=date('j M Y',strtotime($bid['assigned_at']))?></p>
+		        <?php }elseif($bid['bid_status']=='Bid' && !in_array(strtolower($bid['status']),array('open','reject'))){//if i lost the biod?>
+		        <p>Sorry! you lost this bid. <a href="#">How to win a bid?</a></p>
+		        <?php }elseif($bid['bid_status']=='Bid' && in_array(strtolower($bid['status']),array('open','reject'))){//if still bid is ongoing?>
+		        <p>Average bids: </br><b style="color:#0FF"><?=round($bid['avg_time'])?></b> <?=str_replace('dai','day',substr($bid['arrangement'],0,-2))?>s for USD <b style="color:#0FF"><?=round($bid['avg_cost'])?></b>/<?=str_replace('dai','day',substr($bid['arrangement'],0,-2))?></p>
+		        <?php }?>
+		      </div>
+		      <?php if(($bid['bid_status']=='Bid')||($bid['bid_status']=='Accepted' && in_array(strtolower($bid['status']),array('open','reject')))){?>
+		      <div id="stop-following-bid"><a class="unwatch_bid" title="Cancels your bid and stops following on this mission" href="javascript:void(0)" id="unwatch_<?=$bid['bid_id']?>">Stop following</a></div>
+		      <?php }?>
+		    </div>
+		  </div>
+		  <?php endforeach;?>
+		</div>
+		<?php if(in_array($this->session->userdata('role'),array('admin','po'))){?>
+			<ul>
+			<?php foreach($troops as $troop):?>
+				<li><a href="/missions/applicants/<?=$troop['work_id']?>"><?=$troop['num']?> troop(s) applied for mission '<?=$troop['title']?>'</a>.</li>
+			<?php endforeach;?>
+		    </ul>
+		<?php }?>
+		<!-- Page ends -->
+		
+	</div>
 </div>
-<?php if(in_array($this->session->userdata('role'),array('admin','po'))){?>
-	<ul>
-	<?php foreach($troops as $troop):?>
-		<li><a href="/missions/applicants/<?=$troop['work_id']?>"><?=$troop['num']?> troop(s) applied for mission '<?=$troop['title']?>'</a>.</li>
-	<?php endforeach;?>
-    </ul>
-<?php }?>
+
 <script type="text/javascript">
 $(function(){
 		$(".fancybox").fancybox({type : 'iframe'});
