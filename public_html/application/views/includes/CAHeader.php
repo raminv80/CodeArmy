8dc4889aade2e388bc259acfaf4475a94dbdfcbc
mohdhazwan <hello@mohdhazwan.com>
2012-10-11 11:42:58 +0800

    New menu from BY applied to both

--- public_html/application/views/includes/CAHeader.php
+++ public_html/application/views/includes/CAHeader.php
@@ -41,7 +41,405 @@
 <script type="text/javascript" src="/public/js/jscrollpane.js"></script>
 <script type="text/javascript" src="/public/js/codeArmy/jquery.dotdotdot-1.5.1.js"></script>
 <script type="text/javascript" src="/public/js/codeArmy/nanoscroller.js"></script>
+<script src="/public/js/codeArmy/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="/public/js/codeArmy/horizontalNav.js" type="text/javascript" charset="utf-8"></script>
+<script src="/public/js/codeArmy/cookie.js" type="text/javascript" charset="utf-8"></script>
+
 <title><?=$window_title?></title>
+
+<style type="text/css">
+
+	.topnav {margin-top:10px;text-align:center;}
+	.topnav li {width:24.3% !important; float:left; cursor:pointer}
+	/* .topnav li:last-child {position:relative;} */
+	.topnav li a {
+		color:#ff8000; font-size:1em; height: 40px; padding:10px 0;margin-left:3px; 
+		text-shadow:0 1px 1px black; box-shadow:inset 0 1px 1px #666; position:relative;
+		border-radius:5px; border:1px solid #111;
+		background: rgb(69,72,77); /* Old browsers */
+		background: -moz-linear-gradient(top, rgba(69,72,77,1) 0%, rgba(0,0,0,1) 100%); /* FF3.6+ */
+		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(69,72,77,1)), color-stop(100%,rgba(0,0,0,1))); /* Chrome,Safari4+ */
+		background: -webkit-linear-gradient(top, rgba(69,72,77,1) 0%,rgba(0,0,0,1) 100%); /* Chrome10+,Safari5.1+ */
+		background: -o-linear-gradient(top, rgba(69,72,77,1) 0%,rgba(0,0,0,1) 100%); /* Opera 11.10+ */
+		background: -ms-linear-gradient(top, rgba(69,72,77,1) 0%,rgba(0,0,0,1) 100%); /* IE10+ */
+		background: linear-gradient(to bottom, rgba(69,72,77,1) 0%,rgba(0,0,0,1) 100%); /* W3C */
+		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#45484d', endColorstr='#000000',GradientType=0 ); /* IE6-9 */}
+	.topnav li a:hover, .topnav li a.active {
+		background: rgb(255,128,0); /* Old browsers */
+		background: -moz-linear-gradient(top, rgba(255,128,0,1) 0%, rgba(147,88,0,1) 100%); /* FF3.6+ */
+		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,128,0,1)), color-stop(100%,rgba(147,88,0,1))); /* Chrome,Safari4+ */
+		background: -webkit-linear-gradient(top, rgba(255,128,0,1) 0%,rgba(147,88,0,1) 100%); /* Chrome10+,Safari5.1+ */
+		background: -o-linear-gradient(top, rgba(255,128,0,1) 0%,rgba(147,88,0,1) 100%); /* Opera 11.10+ */
+		background: -ms-linear-gradient(top, rgba(255,128,0,1) 0%,rgba(147,88,0,1) 100%); /* IE10+ */
+		background: linear-gradient(to bottom, rgba(255,128,0,1) 0%,rgba(147,88,0,1) 100%); /* W3C */
+		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ff8000', endColorstr='#935800',GradientType=0 ); /* IE6-9 */
+		color:black; text-shadow: 0 -1px 1px #ff8000
+		}	
+	.topnav li a i {font-size:1.6em}
+	.topnav li:first-child {margin-left:0}
+	.topnav li a.mid {height:40px; padding-top:15px; margin-top:-3px;}
+	.topnav li a.mid:hover {
+		background: rgb(18,88,158); /* Old browsers */
+		background: -moz-linear-gradient(top, rgba(18,88,158,1) 0%, rgba(53,106,160,1) 100%); /* FF3.6+ */
+		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(18,88,158,1)), color-stop(100%,rgba(53,106,160,1))); /* Chrome,Safari4+ */
+		background: -webkit-linear-gradient(top, rgba(18,88,158,1) 0%,rgba(53,106,160,1) 100%); /* Chrome10+,Safari5.1+ */
+		background: -o-linear-gradient(top, rgba(18,88,158,1) 0%,rgba(53,106,160,1) 100%); /* Opera 11.10+ */
+		background: -ms-linear-gradient(top, rgba(18,88,158,1) 0%,rgba(53,106,160,1) 100%); /* IE10+ */
+		background: linear-gradient(to bottom, rgba(18,88,158,1) 0%,rgba(53,106,160,1) 100%); /* W3C */
+		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#12589e', endColorstr='#356aa0',GradientType=0 ); /* IE6-9 */	color:white; text-shadow: 0 1px 1px black;
+	}
+	.topnav li a.mid i {font-size:2.4em}
+	.topnav li a.profile-setting {
+		position:absolute; width: 30px; right:0; top:0; border-left:1px solid rgba(255,255,255,.1); 
+		background: rgb(69,72,77); /* Old browsers */
+		background: -moz-linear-gradient(top, rgba(69,72,77,1) 0%, rgba(0,0,0,1) 100%); /* FF3.6+ */
+		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(69,72,77,1)), color-stop(100%,rgba(0,0,0,1))); /* Chrome,Safari4+ */
+		background: -webkit-linear-gradient(top, rgba(69,72,77,1) 0%,rgba(0,0,0,1) 100%); /* Chrome10+,Safari5.1+ */
+		background: -o-linear-gradient(top, rgba(69,72,77,1) 0%,rgba(0,0,0,1) 100%); /* Opera 11.10+ */
+		background: -ms-linear-gradient(top, rgba(69,72,77,1) 0%,rgba(0,0,0,1) 100%); /* IE10+ */
+		background: linear-gradient(to bottom, rgba(69,72,77,1) 0%,rgba(0,0,0,1) 100%); /* W3C */
+		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#45484d', endColorstr='#000000',GradientType=0 ); /* IE6-9 */ 
+		border-radius:0;
+		-webkit-border-top-right-radius: 5px;
+		-webkit-border-bottom-right-radius: 5px;
+		-moz-border-radius-topright: 5px;
+		-moz-border-radius-bottomright: 5px;
+		border-top-right-radius: 5px;
+		border-bottom-right-radius: 5px;
+		color: #ff8000 !important;
+		}
+	.topnav li a.profile-setting:hover {
+		background: rgb(0,0,0); /* Old browsers */
+		background: -moz-linear-gradient(top, rgba(0,0,0,1) 0%, rgba(69,72,77,1) 100%); /* FF3.6+ */
+		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,1)), color-stop(100%,rgba(69,72,77,1))); /* Chrome,Safari4+ */
+		background: -webkit-linear-gradient(top, rgba(0,0,0,1) 0%,rgba(69,72,77,1) 100%); /* Chrome10+,Safari5.1+ */
+		background: -o-linear-gradient(top, rgba(0,0,0,1) 0%,rgba(69,72,77,1) 100%); /* Opera 11.10+ */
+		background: -ms-linear-gradient(top, rgba(0,0,0,1) 0%,rgba(69,72,77,1) 100%); /* IE10+ */
+		background: linear-gradient(to bottom, rgba(0,0,0,1) 0%,rgba(69,72,77,1) 100%); /* W3C */
+		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#000000', endColorstr='#45484d',GradientType=0 ); /* IE6-9 */color:#ff8000;
+	}
+	.topnav li .avatar {
+		width:50px; height:50px; margin-top:-5px; overflow:hidden; border-radius:25%; border:1px solid rgba(255,255,255,.1); box-shadow: 0 -1px 1px black; margin-left:10px; margin-right:10px}
+	.topnav li .avatar img {width:105%}
+	
+	.topnav .child {
+		width:776px;
+		margin-top:5px;
+		display:none;
+		position:absolute; right:40px;
+	}
+	.topnav .child ul {
+	    float: left;
+	    text-align: center;
+	}
+	.topnav .child ul li {
+	    float: left;
+	}
+	.topnav .child ul li a {
+	    display: block;
+		background:black;
+		color: orange;
+		text-shadow:0 1px 1px black;
+	    padding: 8px 20px;
+		height: 20px;
+		line-height:20px;
+	}
+	.topnav .child ul li a:hover {
+	    background: orange;
+		color: black;
+		text-shadow:none;
+	}
+
+	.topnav .arrow_box {
+		position: absolute;
+		background: #111;
+		border: 2px solid #333;
+		width:110px;
+		top:68px;
+		right:-40px;
+		padding:2px 5px;
+		text-align:left !important;
+		display:none;
+		z-index:9266;
+	}
+	.topnav .arrow_box i {font-size:.9em}
+	.topnav .arrow_box a, .topnav .arrow_box a:hover, .topnav .arrow_box a.active {background:none; border:none; text-shadow:none; box-shadow:none; font-size:.85em; border-radius:0; text-transform: uppercase}
+	.topnav .arrow_box a {color:#ff8000 !important}
+	.topnav .arrow_box a:hover {color:orange !important}
+	.topnav .arrow_box:after, .topnav .arrow_box:before {
+		bottom: 100%;
+		border: solid transparent;
+		content: " ";
+		height: 0;
+		width: 0;
+		position: absolute;
+		pointer-events: none;
+	}
+
+	.topnav .arrow_box:after {
+		border-color: rgba(17, 17, 17, 0);
+		border-bottom-color: #111;
+		border-width: 5px;
+		left: 50%;
+		margin-left: -5px;
+	}
+	.topnav .arrow_box:before {
+		border-color: rgba(85, 85, 85, 0);
+		border-bottom-color: #333;
+		border-width: 8px;
+		left: 50%;
+		margin-left: -8px;
+	}
+	.topnav .arrow_box hr {margin:2px 0; border-top:1px solid #333; border-bottom:none}
+
+	.logo {background: url(/public/images/codeArmy/mymission/fillter-toolbar.png); height:100px; width:142px; background-position:-16px -15px; margin: 10px 0 0 10px; border-radius:5px; -moz-border-radius:5px; -webkit-border-radius:5px; border:2px solid #111}
+
+	hr {border-top-color:black; border-bottom-color:#444}
+
+	.profile-header .alert {margin-top:5px; margin-bottom:5px; background:none; border:none; color:orange; text-shadow:none; display:none}
+	.profile-header .alert .close {color:white; text-shadow:0 1px 1px black}
+
+	span.notification {
+		position: absolute;
+		top: 0px;
+		right: 60px;
+		background: red;
+		width: 15px;
+		height: 15px;
+		line-height: 15px;
+		border-radius: 50%;
+		box-shadow: 0 1px 2px black;
+		color:white;
+		text-shadow:0 1px 1px black;
+		font-size:.8em;
+	}
+	
+	.topselectwrap {position:relative}
+	.topselectwrap div {position:absolute; color:white; top:9px; right:10px}
+	
+	.topnav select {background:black; -webkit-appearance: none; border:none; height:35px; line-height:35px; width:180px; padding:0 0 0 10px; margin:0; text-shadow:0 1px 1px black; box-shadow:inset 0 1px 1px #666; color:orange; }
+	
+	.topnav input {margin:0; padding:5px}
+	
+	.topnav li a.cmission {
+		background:#2f9cad !important; /* Old browsers */
+		color:white !important;		
+	}
+	.topnav a#search-submit {position:absolute; background:none; border:none; box-shadow:none; top:2px; right:-10px; color:black; text-shadow:none}
+	
+	.topnav .filter div {background:black; height:36px; line-height:36px; text-shadow:0 1px 1px black; box-shadow:inset 0 1px 1px #666; color:orange}
+	.topnav .filter .filterwrap {display:none}
+	
+	/* PO style */
+	.stickynav {width:1000px; margin:0 auto; position:relative; z-index:99999999; padding-bottom:10px; border-bottom:1px solid black}
+	.ajax-frame {display:none}
+</style>
+
+
 </head>
 
 <body>
+
+<div class="container" style="background:url(http://subtlepatterns.com/patterns/dark_wall.png); width:1000px">
+	<div class="row">
+		<!-- New nav -->
+		<div class="stickynav">
+			<div class="container-fluid">
+				<div class="row-fluid">
+					<div class="span2">
+						<?php if($me['role']=='po' || $me['role']=='admin'){?>
+							<a href="/missions/tallent_map"><div class="logo"></div></a>
+						<?php } else { ?>
+							<a href="/missions"><div class="logo"></div></a>
+						<?php } ?>
+					</div>
+					<div class="span10">
+						<ul class="nav topnav">
+							<li class="first">
+								<a href="/messages/inbox"><i class="icon-envelope-alt"></i> <br />Messages
+								<span class="notification <?=isset($myActiveMessages)&&$myActiveMessages>0?'':'hidden' ?>" id="messages-notification"><?=isset($myActiveMessages)?$myActiveMessages:'?' ?></span>
+								</a>
+
+								<nav class="child">
+									<ul class="childnav">
+										<li><a href="/messages/inbox">Inbox</a></li>
+										<li><a href="/messages/compose">Compose</a></li>
+										<li><a href="/messages/sent">Sent</a></li>
+										<li><a href="/messages/trash">Trash</a></li>
+									</ul>
+								</nav>
+							</li>
+							<!-- <li class="first">
+								<a href="/messages/notifications" ><i class="icon-bullhorn"></i> <br />Notifications
+								<span class="notification <?=isset($myActiveNotifications)&&$myActiveNotifications>0?'':'hidden' ?>" id="notifications-notification"><?=$myActiveNotifications?></span>
+								</a>
+							</li> -->
+							<li class="first">
+								<?php if($me['role']=='po' || $me['role']=='admin'){?>
+								<a href="/missions/tallent_map" class="mid"><i class="icon-globe"></i><br />Find Talent</a>
+								<nav class="child">
+									<ul class="childnav">
+										<li>
+											<div class="filter">
+												<div class="all" onClick="javascript:allusers()">All User</div>
+												<div class="filterwrap">
+													<div onClick="javascript:latest()">Contractors</div>
+													<div onClick="javascript:po()">Product Owners</div>
+													<div onClick="javascript:skills()">Skills</div>
+													<div onClick="javascript:classification()">Division</div>
+												</div>
+											</div>
+										</li>
+										<li><a href="/missions/createnew" class="cmission">Create Mission</a></li>
+										<!-- <li><div><input type="search" /></div></li> -->
+										<li>
+											<div id="search-bar" style="position:relative">
+										    <input type="text" name="search" id="search" value="Find missions" style="width:90%"/>
+										    <a title="Search for missions" href="#" id="search-submit">
+												<i class="icon-search"></i>
+												<img id="search-loader" title="Notifications" src="/public/images/newloader.gif" />
+											</a>
+										  </div>
+										</li>
+									</ul>
+								</nav>
+								<? } else { ?>
+								<a href="/missions" class="mid"><i class="icon-globe"></i><br />Find Missions</a>
+								<nav class="child">
+									<ul class="childnav">
+										<li>
+											<div class="filter">
+												<div class="all" onClick="javascript:allusers()">All User</div>
+												<div class="filterwrap">
+													<div onClick="javascript:classification()">Classification</div>
+													<div onClick="javascript:skills()">Skills</div>
+													<div onClick="javascript:estimation()">Time Estimation</div>
+													<div onClick="javascript:payout()">Payout</div>
+												</div>
+											</div>
+										</li>
+										<!-- <li><a rel="/missions/createnew" class="cmission">Create Mission</a></li> -->
+										<!-- <li><div><input type="search" /></div></li> -->
+										<li>
+											<div id="search-bar" style="position:relative">
+										    <input type="text" name="search" id="search" value="Find missions" style="width:90%"/>
+										    <a title="Search for missions" href="#" id="search-submit">
+												<i class="icon-search"></i>
+												<img id="search-loader" title="Notifications" src="/public/images/newloader.gif" />
+											</a>
+										  </div>
+										</li>
+									</ul>
+								</nav>
+								<?php } ?>
+								
+							</li>
+							<li class="first"><a href="/missions/my_missions"><i class="icon-folder-open"></i> <br />My Mission
+								<span class="notification <?=isset($myActiveMissions)&&$myActiveMissions>0?'':'hidden'?>" id="mymissions-notification"><?=isset($myActiveMissions)?$myActiveMissions:'?'?></span>
+								</a>
+								<nav class="child">
+									<ul class="childnav">
+										<li><a href="/missions/completed">In progress</a></li>
+										<li><a href="/missions/completed">Pending</a></li>
+										<li><a href="/missions/completed">Drafts</a></li>
+										<li><a href="/missions/completed">Completed</a></li>
+										<li><a href="/missions/completed">Deleted</a></li>
+									</ul>
+								</nav>
+							</li>
+							<li class="first">
+								<a href="/profile">
+										<div class="pull-left">
+											<div class="avatar" style="background: url(http://avatars.io/auto/<?=$me["email"]?>) no-repeat center center"></div>
+										</div>
+										<div class="pull-left" style="text-align:left">
+											<p><strong style="text-transform:uppercase"><?=substr($me["username"],0,15);?></strong> <br />
+											<span style="font-size:.8em"><?=$myProfile["specialization"]?></span></p>
+										</div>
+								</a>
+								<nav class="child">
+									<ul class="childnav">
+										<li><a href="/profile">Edit</a></li>
+										<li><a href="/profile">Change password</a></li>
+										<li><a href="/login/logout">Log out</a></li>
+									</ul>
+								</nav>
+								<!-- <a class="profile-setting"><i class="icon-caret-down"></i></a>
+								<div class="arrow_box">
+									<a href="/about"><i class="icon-briefcase"></i> About Us</a> <br />
+									<a href="/faq"><i class="icon-info-sign"></i> FAQ's</a> <br />
+									<a href="/invite"><i class="icon-share"></i> Invite Friends</a>
+									<hr />
+									<a href="/login/logout"><i class="icon-signout"></i> Log Out</a>
+								</div> -->
+							</li>
+						</ul>
+						<div class="clearfix"></div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Ends new nav -->
+	</div>
+</div>
+	
+<script type="text/javascript">
+	//Nav script by MHN
+	$(function(){
+		//New nav function
+		
+		var cname = 'nav', 
+		chname = 'child',
+		coption = {expires: 1, path: '/'},
+		cook = $.cookie(cname),
+		chook = $.cookie(chname);
+		console.log(chook);
+		
+		if (!cook){ 
+			$('.topnav > li').eq(0).find('a').addClass('active');
+		} else {
+			$('.topnav > li').eq(cook).find('a').addClass('active');
+			$('.topnav > li').eq(cook).find('.child').show().find('.childnav a').removeClass('active');
+			
+			if(!chook){
+				$('.topnav .childnav li').eq(0).find('a').addClass('active');
+			} else {
+				$('.topnav > li').eq(cook).find('.childnav li').eq(chook).find('a').addClass('active');
+			}
+			
+		}
+		
+		$('.topnav li').on({
+			click : function(e){
+				e.preventDefault;
+				var self = $(this), index = self.index();
+				$.cookie(cname, index, coption);
+				$.cookie(chname, null);
+			}
+		})
+		
+		$('.topnav .childnav li').on({
+			click : function(){
+				var self = $(this), index = self.index();
+				$.cookie(chname, index, coption);
+			}
+		})
+		
+		$('.profile-setting').on({
+			click : function(){
+				$('.arrow_box').slideToggle();
+			}
+		});
+		
+		$('.filter .all').mouseenter(function(){
+			$(this).next().slideDown('fast');
+		});	
+		
+		$('.filter .filterwrap').mouseleave(function(){
+			$(this).slideUp('fast');
+		});
+		
+		$('.child').horizontalNav();
+	});
+</script>
